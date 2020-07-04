8736f5b09e22d8594017b8a456d81ed34f036c09
Jogli Martins <joglimartins@gmail.com>
2020-07-04 13:38:48 -0300

    correção de CORS

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://grade-api.herokuapp.com',
+    origin: 'https://desafio-grade-app.herokuapp.com',
   })
 );
 
